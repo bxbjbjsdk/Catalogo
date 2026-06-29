c9361c734da1b6281fb9ff220ab06dadb0cb18f2
Cele <lupistbn@gmail.com>
2026-06-29 08:18:34 -0500

    add:

--- Backend/RYCTech.Api/obj/Debug/net10.0/RYCTech.Api.AssemblyInfo.cs
+++ Backend/RYCTech.Api/obj/Debug/net10.0/RYCTech.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1cddc2399565f1267eae10dc9d97e1c0526ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47aadf0e51920fb5dc90a0d110e454f14b421c8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
