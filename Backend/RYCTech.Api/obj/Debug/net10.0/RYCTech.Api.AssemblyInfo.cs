693795c0e10d9a88414e398443397b39d62615db
RodrigoDBH <daniel.rodrigo.hu@gmail.com>
2026-06-28 22:04:28 -0500

    reestructura

--- Backend/RYCTech.Api/obj/Debug/net10.0/RYCTech.Api.AssemblyInfo.cs
+++ Backend/RYCTech.Api/obj/Debug/net10.0/RYCTech.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1665cb18d91518643675a4b14a554edc1181709e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec6edc7b6a1ea23c7b5ad7b5c49db16408797ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RYCTech.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
