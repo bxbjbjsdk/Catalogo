b61177620efef360f8ea906e36117b54fddfcecd
RodrigoDBH <daniel.rodrigo.hu@gmail.com>
2026-07-03 21:21:57 -0500

    cambio ip

--- Frontend/js/utils.js
+++ Frontend/js/utils.js
@@ -7,7 +7,7 @@
 
 // URL del backend — apunta a tu IP en red local
 // Si cambias de red, actualiza esta IP
-const URL_BACKEND = 'http://172.18.9.152:5000/api';
+const URL_BACKEND = `http://${window.location.hostname}:5000/api`;
 
 /* ------------------------------------------------
    ABRIR MODAL
